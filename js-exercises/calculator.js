2ca60d38d3f583c3b487705eb2975816391c9a8f
Berenguer Pou <berenguer.pou@gmail.com>
2022-01-19 22:27:47 +0100

    refactored function declaration order sqrt(number) getAndCheckInput()

--- js-exercises/calculator.js
+++ js-exercises/calculator.js
@@ -4,27 +4,8 @@ Berenguer Pou
 precourse ISDI Coders 2022-1
 */
 
-function isdiCalculator(limit = 2) {
-  if (typeof limit !== "number" || limit <= 0) {
-    console.log("Limit has to be a number greater than 0!");
-    return;
-  }
-  const numList = []; // we will store valid numbers in an array to make it reusable for calculator-pro project with minor changes.
-  let onlyOneNumber = false;
-  let numListMsg = "Numbers in the list: ";
-  console.log("Hello! ISDICalculator started!");
-  while (!onlyOneNumber && numList.length < limit) {
-    const number = getAndCheckInput();
-    if (number !== null) {
-      numList.push(number);
-      console.log((numListMsg += number + " ")); // logs the list of accumulated valid numbers
-    } else if (numList.length === 0) {
-      console.log("Bye! You have cancelled ISDI Calculator");
-      return; // stops the program if user escapes before having at least one number in the list
-    } else if (numList.length === 1) onlyOneNumber = true;
-  }
-  makeCalculations(numList);
-  console.log("Bye! ISDI Calculator has ended calculations");
+function sqrt(number) {
+  return Math.sqrt(number);
 }
 
 function getAndCheckInput() {
@@ -33,7 +14,7 @@ function getAndCheckInput() {
   while (value === undefined) {
     const input = prompt("Type a number:");
     if (input === null) return null;
-    else if (isNaN(Number(input)) || input.trim() === "")
+    if (isNaN(Number(input)) || input.trim() === "")
       input.trim() === "" // we handle empty values as invalid inputs because "" would throw 0! (but if user types 0 is OK.)
         ? alert("Value is empty! Please type a number.")
         : alert(`${input} is not a valid number. Please type it again!`);
@@ -79,8 +60,27 @@ function makeCalculations(arrNumbers) {
   }
 }
 
-function sqrt(number) {
-  return Math.sqrt(number);
+function isdiCalculator(limit = 2) {
+  if (typeof limit !== "number" || limit <= 0) {
+    console.log("Limit has to be a number greater than 0!");
+    return;
+  }
+  const numList = []; // we will store valid numbers in an array to make it reusable for calculator-pro project with minor changes.
+  let onlyOneNumber = false;
+  let numListMsg = "Numbers in the list: ";
+  console.log("Hello! ISDICalculator started!");
+  while (!onlyOneNumber && numList.length < limit) {
+    const number = getAndCheckInput();
+    if (number !== null) {
+      numList.push(number);
+      console.log((numListMsg += `${number} `)); // logs the list of accumulated valid numbers
+    } else if (numList.length === 0) {
+      console.log("Bye! You have cancelled ISDI Calculator");
+      return; // stops the program if user escapes before having at least one number in the list
+    } else if (numList.length === 1) onlyOneNumber = true;
+  }
+  makeCalculations(numList);
+  console.log("Bye! ISDI Calculator has ended calculations");
 }
 
 isdiCalculator(2); // we call the main function with a limit of 2, as required in proyecto calculator, but any postive number works.
