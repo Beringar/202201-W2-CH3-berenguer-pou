9c86947329ef6c1e0ec27a6fb3ab3349cd0bb545
Berenguer Pou <berenguer.pou@gmail.com>
2022-01-20 03:34:22 +0100

    refactored airlines.js

--- js-exercises/airlines.js
+++ js-exercises/airlines.js
@@ -1,10 +1,11 @@
+/* eslint-disable no-alert */
 /*  
 proyecto airlines
 Berenguer Pou
 precourse ISDI Coders 2022-1
 */
 
-const flights = [
+const mockFlights = [
   { id: 123, to: "Bilbao", from: "Barcelona", cost: 58, scale: false },
   { id: 287, to: "New York", from: "Barcelona", cost: 490, scale: false },
   { id: 333, to: "Los Angeles", from: "Madrid", cost: 800, scale: true },
@@ -17,33 +18,23 @@ const flights = [
   { id: 521, to: "Sydney", from: "Barcelona", cost: 1620, scale: true },
   { id: 32, to: "Tel-Aviv", from: "Madrid", cost: 630, scale: false },
 ];
+function getPromptInput(msg, errorMsg) {
+  let input;
+  let validInput;
+  do {
+    input = prompt(msg);
+    if (input === null) return null;
+    if (/^\S*$/.test(input)) {
+      validInput = input;
+    } else {
+      alert(errorMsg);
+    }
+  } while (!validInput);
+  return validInput;
+}
 
-function airlines() {
-  console.log(`💻 Airlines Console started...`);
-  const username = getPromptInput(
-    "Please, enter your username:",
-    "Incorrect name! You must enter a valid name."
-  );
-  if (username === null) {
-    console.log("⏹ You have stopped Airlines Console. Bye!");
-    return;
-  }
-  console.log(`👋 Welcome ${username}!`);
-  printFlights(flights);
-  console.log(
-    `ℹ️ The average price of available flights is: ${getAveragePrice(
-      flights
-    ).toFixed(2)}€`
-  );
-  console.log(
-    `ℹ️ Flights with stopovers: ${flights.reduce((acc, flight) => {
-      if (flight.scale) acc += 1;
-      return acc;
-    }, 0)}`
-  );
-  console.log("ℹ️ These are the last 5 destinations of scheduled flights:");
-  printLastDestinations(5);
-  console.log("⏹ End of Airlines Console. Bye!");
+function sortFlights(flights) {
+  return flights.slice().sort((a, b) => a.id - b.id);
 }
 
 function getAveragePrice(flights) {
@@ -64,26 +55,40 @@ function printFlights(flights) {
     );
   });
 }
-
-function sortFlights(flights) {
-  return flights.slice().sort((a, b) => a.id - b.id);
-}
-
 function printLastDestinations(numFlights) {
-  const lastFlights = sortFlights(flights).filter(
+  const lastFlights = sortFlights(mockFlights).filter(
     (item, index, array) => index >= array.length - numFlights
   );
   console.table(lastFlights, ["to", "id"]);
 }
 
-function getPromptInput(msg, errorMsg) {
-  let input, validInput;
-  do {
-    input = prompt(msg);
-    if (input === null) return null;
-    /^\S*$/.test(input) ? (validInput = input) : alert(errorMsg);
-  } while (!validInput);
-  return validInput;
+function airlines() {
+  console.log(`💻 Airlines Console started...`);
+  const username = getPromptInput(
+    "Please, enter your username:",
+    "Incorrect name! You must enter a valid name."
+  );
+  if (username === null) {
+    console.log("⏹ You have stopped Airlines Console. Bye!");
+    return;
+  }
+  console.log(`👋 Welcome ${username}!`);
+  printFlights(mockFlights);
+  console.log(
+    `ℹ️ The average price of available flights is: ${getAveragePrice(
+      mockFlights
+    ).toFixed(2)}€`
+  );
+  console.log(
+    `ℹ️ Flights with stopovers: ${mockFlights.reduce((acc, flight) => {
+      let accumulator = acc;
+      if (flight.scale) accumulator += 1;
+      return accumulator;
+    }, 0)}`
+  );
+  console.log("ℹ️ These are the last 5 destinations of scheduled flights:");
+  printLastDestinations(5);
+  console.log("⏹ End of Airlines Console. Bye!");
 }
 
 airlines();
